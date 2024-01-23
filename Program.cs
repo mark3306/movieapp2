0283934710d537b4d5b9d29670e087f5891b9068
Mark Westover <skyforwarddesign@gmail.com>
2024-01-23 17:08:57 -0500

    bring seed data back

--- Program.cs
+++ Program.cs
@@ -21,6 +21,13 @@ builder.Logging.AddAzureWebAppDiagnostics();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
